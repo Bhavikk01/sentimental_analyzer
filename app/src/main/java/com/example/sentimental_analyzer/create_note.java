03c922a93ad3688f63a8e1113b0012b1a969f81e
Anuroop Farkya <anuroopf02@gmail.com>
2023-01-07 10:57:47 +0530

    intial commit

---
+++ app/src/main/java/com/example/sentimental_analyzer/create_note.java
@@ -0,0 +1,14 @@
+package com.example.sentimental_analyzer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class create_note extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_note);
+    }
+}
